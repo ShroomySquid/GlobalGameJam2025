ca57b7353baeba2ac21d329a3f52e9f64ef457f2
Felix <f.a.r.barrette@hotmail.com>
2025-01-25 10:54:15 -0500

    Fixed pathing in game retry btn

--- Scenes/CoreScenes/Game/game.gd
+++ Scenes/CoreScenes/Game/game.gd
@@ -43,4 +43,4 @@ func _on_quit_btn_pressed():
 	get_tree().quit()
 
 func _on_retry_btn_pressed():
-	get_tree().change_scene_to_file("res://Scenes/CoreScenes/game.tscn")
+	get_tree().change_scene_to_file("res://Scenes/CoreScenes/Game/game.tscn")
